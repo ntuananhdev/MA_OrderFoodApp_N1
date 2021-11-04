87eacb1a1e3369f722082903387603840ff1e29d
Tuấn Anh <80771459+ntuananhdev@users.noreply.github.com>
2021-11-04 11:17:48 +0700

    Initial commit

---
+++ app/src/main/java/com/example/ma_orderfoodapp_n1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.ma_orderfoodapp_n1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
